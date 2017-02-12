f7af1bdc246fcecc97e40a24de43201038c9c136
Hans Vorsteveld <hvorsteveld@gmail.com>
2017-02-12 17:28:08 -0500

    Upgrade to bootstrap 4. add newWine page (still empty

--- app/views.py
+++ app/views.py
@@ -1,19 +1,22 @@
 from app import app
 from flask import render_template
 from Models import WineBottle, db
+from sqlalchemy import func
 
 @app.route('/')
 @app.route('/index')
 def index():
-    x = "";
-    bottles = WineBottle.query.all()
-    for row in bottles:
-        x += "<br>" + row.Name
 
-    return render_template("index.html", bottles=bottles)
+    bottleCount = db.session.query(func.count(WineBottle.id)).scalar()
+
+    return render_template("index.html", bottleCount=bottleCount)
 
 
 @app.route('/viewAllWines')
 def viewAllWines():
     bottles = WineBottle.query.all()
     return render_template("viewAllWines.html", bottles=bottles)
+
+@app.route('/newWine')
+def newWine():
+    return render_template("newWine.html")
