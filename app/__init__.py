9e7726ed50496c6b194ede1e97a709f67061ea2f
Hans Vorsteveld <hvorsteveld@gmail.com>
2017-02-12 17:33:02 -0500

    move configs to config.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,7 @@ from flask import Flask
 from Models import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://hans:1T5qpn00@MSSQLWine'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object('config')
 
 db.init_app(app)
 
