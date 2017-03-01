59a3cc1251e5892f558095e537e8e0cd92c76d37
Hans Vorsteveld <hvorsteveld@gmail.com>
2017-02-28 22:32:35 -0500

    Basic add new wine functionality

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express');
-var proxy = require('express-http-proxy');
-var sql = require('seriate');
-var app = express();
+var form = require('express-form');
+var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
+var sql = require('seriate');
+
+var app = express();
+app.use(express.static('static'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 var config = {
     'server': 'localhost',
@@ -18,7 +23,6 @@ nunjucks.configure('templates', {
 });
 
 
-app.use(express.static('static'));
 
 app.get('/', function(req, res) {
     sql.execute( {
@@ -37,7 +41,7 @@ app.get('/viewAllWines', function(req, res) {
             query: sql.fromFile('./sql/getAllWinesDetail.sql')
         }).then( function( results ){
             console.log(results);
-            res.render('viewAllWines.html', {bottles: results} );
+            res.render('Wine/ViewAllWines.html', {bottles: results} );
         }, function( err ){
             console.log("Something bad happened:", err);
             res.send("ERROR");
@@ -45,8 +49,42 @@ app.get('/viewAllWines', function(req, res) {
 });
 
 app.get('/newWine', function(req, res){
-    res.render('newWine.html');
+    res.render('Wine/NewWine.html');
 });
+
+app.post('/newWine', 
+    form(
+        form.filter("name").required().trim(),
+        form.filter("year").required().isNumeric(),
+        form.filter("type").required().trim(),
+        form.filter("region").required().trim(),
+        form.filter("rating").required().isNumeric()
+     ),
+    function(req, res){
+        console.log(req.form);
+        if(req.form.isValid){
+            sql.execute( {
+                query: sql.fromFile('./sql/AddWine.sql'),
+                params: {
+                    Name: {type: sql.VARCHAR, val: req.form.name },
+                    Year: {type: sql.INT, val: req.form.year },
+                    Type: {type: sql.VARCHAR, val: req.form.type },
+                    Region: {type: sql.VARCHAR, val: req.form.region },
+                    Comments: {type: sql.VARCHAR, val: req.form.comments },
+                    Rating: { type: sql.TINYINT, val: req.form.rating }
+                }
+            }).then( function( results ){
+                console.log(results);
+                res.render('Wine/ViewAllWines.html', {bottles: results})
+            }, function( err ){
+                console.log("Something bad happened on insert");
+                res.send("ERROR");
+            });
+        }else{
+            res.send("INVALID ENTRIES, TRY AGAIN");
+        }
+    }
+);
 
 var _port = 5000;
 app.listen(_port, function(){
---
+++ sql/AddWine.sql
@@ -0,0 +1,4 @@
+INSERT INTO
+    Data.Wines ( [Name], [Year], [Type], [Region], [Comments], [Rating] )
+VALUES
+    (@Name, @Year, @Type, @Region, @Comments, @Rating)
