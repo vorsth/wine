f5a6217d097d42634a0fd4bca90cfc398670cd92
Hans Vorsteveld <hvorsteveld@gmail.com>
2017-03-16 03:25:12 +0000

    Add https support

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var bodyParser = require('body-parser');
 var config = require('config');
-var express = require('express');
 var form = require('express-form');
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
 var nunjucks = require('nunjucks');
 var pgp = require('pg-promise')();
+
+var privateKey = fs.readFileSync('../sslcert/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('../sslcert/cert.pem', 'utf8');
+var credentials = { key: privateKey, cert: certificate };
+var express = require('express');
 
 var app = express();
 app.use(express.static('static'));
@@ -103,8 +110,17 @@ function handleError(error){
   return res.send('OTHER ERROR');
 }
 
-var _port = config.get('Http.Port');
-app.listen(_port, "0.0.0.0"
-, function(){
-    console.log(`App listening on port ${_port}`)
+var _httpPort = config.get('Http.Port');
+var _httpsPort = config.get('Https.Port');
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(_httpPort, "0.0.0.0", function(){
+  console.log(`HTTP listening on port ${_httpPort}`)
 });
+
+httpsServer.listen(_httpsPort, "0.0.0.0", function(){
+  console.log(`HTTPS listening on port ${_httpsPort}`)
+});
+
