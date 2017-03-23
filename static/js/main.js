2e536b062338d9ef45754b655380f3699b8ef684
Hans Vorsteveld <hvorsteveld@gmail.com>
2017-03-23 04:52:27 +0000

    spelling

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 requirejs(['jquery','tether','bootstrap','googleSignIn','login'], function(jquery, tether, bootstrap, googleSignIn, login){
   console.log("Require Loaded");
 
-  // Makke table rows clickable by responding to the data-href attribute
+  // Make table rows clickable by responding to the data-href attribute
   $(function(){
       $('.table tbody tr[data-href]').each(function(){
         $(this).css('cursor','pointer').hover(
